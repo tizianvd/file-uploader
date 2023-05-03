242473cd180953d13aae63bce45128363c2084d4
Tizian <tizianvd@gmail.com>
2023-05-03 08:04:45 +0200

    Initial commit

---
+++ apps/file-uploader/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'file-uploader-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'file-uploader';
+}
